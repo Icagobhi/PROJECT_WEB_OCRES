a7e4dfa55561fd0ee4036f7b84604d36ccf91b9c
Lucas <lvagner77@gmail.com>
2021-10-31 00:45:59 +0200

    upgrade

---
+++ frontend/build/precache-manifest.318b9b34b41cc725197b69b742414015.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e220cc8af58d729c97982a8ff74c8a9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11cf0fa795dc8bf3616b",
+    "url": "/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "813eb763244f79a0b5a6",
+    "url": "/static/js/2.161bd094.chunk.js"
+  },
+  {
+    "revision": "11cf0fa795dc8bf3616b",
+    "url": "/static/js/main.a2d037b2.chunk.js"
+  },
+  {
+    "revision": "ed4750f41f4543566353",
+    "url": "/static/js/runtime-main.fbe98da3.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
